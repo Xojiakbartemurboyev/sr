e1c98ba3c679d6943c757ff772479cff656e5745
Xojiakbartemurboyev <notxoji@gmail.com>
2025-04-04 15:32:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let numbers = [2, 4, 9, 10, 45, 67, 8, 90];
+console.log(numbers, 'boshlanishdagi array');
+
+
+let evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers, 'juft sonlarga ajratilgan array');
